087fbdb24f5089998941547928ef42462c90f415
Terrence Yang <encoreyang78@gmail.com>
2019-11-03 14:27:48 +0800

    add port

--- app.js
+++ app.js
@@ -4,19 +4,27 @@ const db = require('./models')
 const bodyParser = require('body-parser')
 const session = require('express-session')
 const flash = require('connect-flash')
+const passport = require('./config/passport')
+const methodOverride = require('method-override')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
 app.use(flash())
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
+//靜態圖片路徑
+app.use('/upload', express.static(__dirname + '/upload'))
 
 // 把 req.flash 放到 res.locals 裡面
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = req.user
   next()
 })
 
@@ -26,4 +34,4 @@ app.listen(port, () => {
 })
 
 // 引入 routes 並將 app 傳進去，讓 routes 可以用 app 這個物件來指定路由
-require('./routes')(app)
+require('./routes')(app, passport)
