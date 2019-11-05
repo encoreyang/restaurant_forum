bb23f2ce43487a5a86f3db821fd08dcd00ee322e
Terrence Yang <encoreyang78@gmail.com>
2019-11-05 21:27:46 +0800

    add imgur

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ const session = require('express-session')
 const flash = require('connect-flash')
 const passport = require('./config/passport')
 const methodOverride = require('method-override')
+
 const app = express()
 const port = process.env.PORT || 3000
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
