f660f7a4663f09a2138e1092a745fc4e0d5179fc
Terrence Yang <encoreyang78@gmail.com>
2019-11-09 16:14:43 +0800

    Add admin categories edit and put feature

--- controllers/catecoryController.js
+++ controllers/catecoryController.js
@@ -3,7 +3,14 @@ const Category = db.Category
 let categoryController = {
   getCategories: (req, res) => {
     return Category.findAll().then(categoies => {
-      return res.render('admin/categories', { categoies: categoies })
+      if (req.params.id) {
+        Category.findByPk(req.params.id)
+          .then((category) => {
+            return res.render('admin/categories', { categoies: categoies, category: categoies })
+          })
+      } else {
+        return res.render('admin/categories', { categoies: categoies })
+      }
     })
   },
 
@@ -17,6 +24,21 @@ let categoryController = {
       })
         .then((category) => {
           res.redirect('/admin/categories')
+        })
+    }
+  },
+  //修改資料Controller action
+  putCategory: (req, res) => {
+    if (!req.body.name) {
+      req.flash('error_messages', 'name didn\'t exist')
+      return res.redirect('back')
+    } else {
+      return Category.findByPk(req.params.id)
+        .then((category) => {
+          category.update(req.body)
+            .then((category) => {
+              res.redirect('/admin/categories')
+            })
         })
     }
   }
--- routes/index.js
+++ routes/index.js
@@ -40,6 +40,8 @@ module.exports = (app, passport) => {
   //categories
   app.get('/admin/categories', authenticatedAdmin, categoryController.getCategories)
   app.post('/admin/categories', authenticatedAdmin, categoryController.postRestaurant)
+  app.get('/admin/categories/:id', authenticatedAdmin, categoryController.getCategories)
+  app.put('/admin/categories/:id', authenticatedAdmin, categoryController.putCategory)
 
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
