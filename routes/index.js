05f988e7ee02439c8335b6a790ac9e05e13d36d1
Terrence Yang <encoreyang78@gmail.com>
2019-11-16 11:04:12 +0800

    refactor router and GET/api/admin/restaurants

--- routes/index.js
+++ routes/index.js
@@ -1,67 +1,7 @@
-const restController = require('../controllers/restController.js')
-const adminController = require('../controllers/adminController.js')
-const userController = require('../controllers/userController.js')
-const categoryController = require('../controllers/catecoryController.js')
-const commentController = require('../controllers/commentController.js')
-const multer = require('multer')
-const upload = multer({ dest: 'temp/' }) //上傳暫存的資料夾
+let routes = require('./routes')
+let apis = require('./apis')
 
-
-module.exports = (app, passport) => {
-  //驗證方法
-  const authenticated = (req, res, next) => {
-    if (req.isAuthenticated()) {
-      return next()
-    }
-    res.redirect('/signin')
-  }
-  const authenticatedAdmin = (req, res, next) => {
-    if (req.isAuthenticated()) {
-      if (req.user.isAdmin) { return next() }
-      return res.redirect('/')
-    }
-    res.redirect('/signin')
-  }
-
-  //如果使用者訪問首頁，就導向 /restaurants 的頁面
-  app.get('/', authenticated, (req, res) => res.redirect('/restaurants'))
-  //在 /restaurants 底下則交給 restController.getRestaurants 來處理
-  app.get('/restaurants', authenticated, restController.getRestaurants)
-  app.get('/users/top', authenticated, userController.getTopUser)
-  app.get('restaurants/feeds', authenticated, restController.getFeeds)
-  app.get('/restaurants/:id', authenticated, restController.getRestaurant) //顯示單一餐廳
-
-  //加到最愛路由
-  app.post('/favorite/:restaurantId', authenticated, userController.addFavorite)
-  app.delete('/favorite/:restaurantId', authenticated, userController.removeFavorite)
-  //評論路由
-  app.post('/comments', authenticated, commentController.postComment)
-  app.delete('/comments/:id', authenticatedAdmin, commentController.deleteComment)
-  //追蹤路由
-  app.post('/following/:userId', authenticated, userController.addFollowing)
-  app.delete('/following/:userId', authenticated, userController.removeFollowing)
-
-  // 連到 /admin 頁面就轉到 /admin/restaurants
-  app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'))
-  // 在 /admin/restaurants 底下則交給 adminController.getRestaurants 處理
-  app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
-  app.get('/admin/restaurants/create', authenticatedAdmin, adminController.creatRestaurant)  //進入新增頁面
-  app.post('/admin/restaurants', authenticatedAdmin, upload.single('image'), adminController.postRestaurant) //新增一筆餐廳
-  app.get('/admin/restaurants/:id', authenticatedAdmin, adminController.getRestaurant) //看單一筆餐廳資料
-  app.get('/admin/restaurants/:id/edit', authenticatedAdmin, adminController.editRestaurant) //進入編輯頁面
-  app.put('/admin/restaurants/:id', authenticatedAdmin, upload.single('image'), adminController.putRestaurant)
-  app.delete('/admin/restaurants/:id', authenticatedAdmin, adminController.deleteRestaurant)
-  //categories
-  app.get('/admin/categories', authenticatedAdmin, categoryController.getCategories)
-  app.post('/admin/categories', authenticatedAdmin, categoryController.postCategory)
-  app.get('/admin/categories/:id', authenticatedAdmin, categoryController.getCategories)
-  app.put('/admin/categories/:id', authenticatedAdmin, categoryController.putCategory)
-  app.delete('/admin/categories/:id', authenticatedAdmin, categoryController.deleteCategory)
-
-  app.get('/signup', userController.signUpPage)
-  app.post('/signup', userController.signUp)
-
-  app.get('/signin', userController.signInPage)
-  app.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
-  app.get('/logout', userController.logout)
+module.exports = (app) => {
+  app.use('/', routes)
+  app.use('/api', apis)
 }
---
+++ routes/routes.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const router = express.Router();
+
+const passport = require('../config/passport')
+
+const restController = require('../controllers/restController.js')
+const adminController = require('../controllers/adminController.js')
+const userController = require('../controllers/userController.js')
+const categoryController = require('../controllers/catecoryController.js')
+const commentController = require('../controllers/commentController.js')
+const multer = require('multer')
+const upload = multer({ dest: 'temp/' }) //上傳暫存的資料夾
+
+
+//驗證方法
+const authenticated = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next()
+  }
+  res.redirect('/signin')
+}
+const authenticatedAdmin = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    if (req.user.isAdmin) { return next() }
+    return res.redirect('/')
+  }
+  res.redirect('/signin')
+}
+
+//如果使用者訪問首頁，就導向 /restaurants 的頁面
+router.get('/', authenticated, (req, res) => res.redirect('/restaurants'))
+//在 /restaurants 底下則交給 restController.getRestaurants 來處理
+router.get('/restaurants', authenticated, restController.getRestaurants)
+router.get('/users/top', authenticated, userController.getTopUser)
+router.get('restaurants/feeds', authenticated, restController.getFeeds)
+router.get('/restaurants/:id', authenticated, restController.getRestaurant) //顯示單一餐廳
+
+//加到最愛路由
+router.post('/favorite/:restaurantId', authenticated, userController.addFavorite)
+router.delete('/favorite/:restaurantId', authenticated, userController.removeFavorite)
+//評論路由
+router.post('/comments', authenticated, commentController.postComment)
+router.delete('/comments/:id', authenticatedAdmin, commentController.deleteComment)
+//追蹤路由
+router.post('/following/:userId', authenticated, userController.addFollowing)
+router.delete('/following/:userId', authenticated, userController.removeFollowing)
+
+// 連到 /admin 頁面就轉到 /admin/restaurants
+router.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'))
+// 在 /admin/restaurants 底下則交給 adminController.getRestaurants 處理
+router.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
+router.get('/admin/restaurants/create', authenticatedAdmin, adminController.creatRestaurant)  //進入新增頁面
+router.post('/admin/restaurants', authenticatedAdmin, upload.single('image'), adminController.postRestaurant) //新增一筆餐廳
+router.get('/admin/restaurants/:id', authenticatedAdmin, adminController.getRestaurant) //看單一筆餐廳資料
+router.get('/admin/restaurants/:id/edit', authenticatedAdmin, adminController.editRestaurant) //進入編輯頁面
+router.put('/admin/restaurants/:id', authenticatedAdmin, upload.single('image'), adminController.putRestaurant)
+router.delete('/admin/restaurants/:id', authenticatedAdmin, adminController.deleteRestaurant)
+//categories
+router.get('/admin/categories', authenticatedAdmin, categoryController.getCategories)
+router.post('/admin/categories', authenticatedAdmin, categoryController.postCategory)
+router.get('/admin/categories/:id', authenticatedAdmin, categoryController.getCategories)
+router.put('/admin/categories/:id', authenticatedAdmin, categoryController.putCategory)
+router.delete('/admin/categories/:id', authenticatedAdmin, categoryController.deleteCategory)
+
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
+
+router.get('/signin', userController.signInPage)
+router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
+router.get('/logout', userController.logout)
+
+module.exports = router
