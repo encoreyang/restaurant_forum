533ef461effa2c9091f1d8d190248786f2be1ad4
Terrence Yang <encoreyang78@gmail.com>
2019-11-23 16:03:59 +0800

    POST/api/signup

--- controllers/api/userController.js
+++ controllers/api/userController.js
@@ -35,6 +35,33 @@ let userController = {
         }
       })
     })
-  }
+  },
+  signUp: (req, res) => {
+    // confirm password
+    if (req.body.passwordCheck !== req.body.password) {
+      return res.json({ status: 'error', message: '兩次密碼輸入不同！' })
+      // req.flash('error_messages', '兩次密碼輸入不同！')
+      // return res.redirect('/signup')
+    } else {
+      // confirm unique user
+      User.findOne({ where: { email: req.body.email } }).then(user => {
+        if (user) {
+          return res.json({ status: 'error', message: '信箱重複！' })
+          // req.flash('error_messages', '信箱重複！')
+          // return res.redirect('/signup')
+        } else {
+          User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
+          }).then(user => {
+            return res.json({ status: 'success', message: '成功註冊帳號！' })
+            // req.flash('success_messages', '成功註冊帳號！')
+            // return res.redirect('/signin')
+          })
+        }
+      })
+    }
+  },
 }
 module.exports = userController
